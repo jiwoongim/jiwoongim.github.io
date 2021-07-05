a1214a19fe8985e5fea48a91a3fb5e22e9adc557
Jiwoong Im <jiwoongim@Jiwoongs-MBP.cable.rcn.com>
2021-07-05 09:47:35 -0400

    index.php

---
+++ index.php
@@ -0,0 +1,104 @@
+
+<!-- This site was created using the Skeleton Boilerplate 
+http://www.getskeleton.com
+===================================================== -->
+
+<!doctype html>
+<!--[if lt IE 7 ]><html class="ie ie6" lang="en"> <![endif]-->
+<!--[if IE 7 ]><html class="ie ie7" lang="en"> <![endif]-->
+<!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--><html lang="en"> <!--<![endif]-->
+<head>
+
+	<!-- Basic Page Needs
+  ================================================== -->
+	<meta charset="utf-8" />
+	<title>Daniel Jiwoong Im</title>
+	<meta name="description" content="">
+	<meta name="author" content="">
+	<meta name="keywords" content="">
+	
+	<!-- Mobile Specific Metas
+  ================================================== -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" /> 
+	
+	<!-- CSS
+  ================================================== -->
+	<link rel="stylesheet" href="stylesheets/base.css">
+	<link rel="stylesheet" href="stylesheets/skeleton.css">
+	<link rel="stylesheet" href="stylesheets/layout.css">
+
+</head>
+<body>
+
+
+	<!-- Primary Page Layout
+	================================================== -->
+
+
+	<div class="container">	
+	
+		<div class="four columns sidebar" style="margin-top: 80px">
+			<ul>
+				<li><h2>Daniel Jiwoong Im</h2></li>
+                <li><small>University of Montreal <br /> <!--Graduate Student <br />-->
+				Computer Science </small>
+				<br /><br />
+			</ul>
+			<ul id = "contact">
+					<li><strong>imdaniel <em>AT</em> iro.umontreal.ca</strong></li>
+			</ul>
+			<hr />
+			<nav>
+				<ul>
+				<li><a href="index.html">About Daniel</a></li>
+				<li><a href="resume.html">Curriculum Vitae</a></li>
+				<li><a href="research.html">Research</a></li>
+				<li><a href="teaching.html">Talk & Teaching </a></li>
+				<li><a href="code.html">Note & Code</a></li>
+                <!--<li><a href="links.html">Links</a></li>-->
+				</ul>
+			</nav>
+		</div>
+
+		<div class="eleven columns content" style="margin-top: 100px">
+			<h1>About Daniel</h1>	
+			<hr />
+			<img class="scale-with-grid" src="DanielIm.png" align="right">
+            <p> I am a research scientist at Janelia Research Centre,
+            <a href ="https://www.janelia.org/" style="text-decoration:none;">Branson Lab</a> 
+            under the advisory of Kristin Branson.
+           
+            Previously, I was a researcher at the University of Montreal, 
+            <a href ="http://www.mila.umontreal.ca/" style="text-decoration:none;">MILA machine learning institute</a> 
+            under the advisory of 
+            <a href ="http://www.iro.umontreal.ca/~memisevr/" style="text-decoration:none;">Roland Memisevic</a>.</br></br>
+
+            I received my Masters degree from the University of Guelph's School of Engineering department under the advisory of 
+            <a href ="http://www.uoguelph.ca/~gwtaylor/" style="text-decoration:none;">Graham W. Taylor</a>. 
+            I received my undergraduate degree from the University of Toronto in 2012. I completed a specialist program  
+            in Computer Science with a specialization in Artifical Intelligence 
+            Specialist and pursued Mathematics and It's Application specialist program. </br></br>
+
+            My primary interests in working with deep learning models and analyzing unsupervised representatation learning 
+            models under the view of dynamical system or probabilistic perspective. 
+            <!--My primary interests lie in the probabilistic perspective of machine learning and differential geometry view of deep learning.-->
+            </p>
+			<!--
+			<br />
+			<ul class="square">
+				<li>Geometric Modeling</li>
+				<li>Fluid Simulation</li>
+				<li>Elasticity</li>
+			</ul>-->
+			<p>I can be contacted by email at: imdaniel <em>AT</em> iro.umontreal.ca</p>
+			</div>
+
+	</div><!-- container -->
+
+	
+<!-- End Document
+================================================== -->
+</body>
+</html>
+
